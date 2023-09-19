dfda0868598a8ded85422c5cc2fddb6d394578fd
anaismalet <anais.malet55@gmail.com>
2023-09-19 12:03:06 +0200

    third commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=LGCXZQVT0R"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
+</script>
+ """
+ return prefix_google + "Hello World"
+
 from flask import Flask
 
 app = Flask(__name__)
