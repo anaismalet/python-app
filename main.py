eabd69a494777ff81a4b3e0f073db2cd55f4a32d
anaismalet <anais.malet55@gmail.com>
2023-09-19 12:16:14 +0200

    update GA_ID

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=LGCXZQVT0R"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-LGCXZQVT0R"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', 'LGCXZQVT0R');
+ gtag('config', 'G-LGCXZQVT0R');
 </script>
  """
  return prefix_google + "Hello World"
