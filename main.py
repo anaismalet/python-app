14081f85024095b2a480e52ad9fca8a95bbe672b
anaismalet <anais.malet55@gmail.com>
2023-09-19 12:43:09 +0200

    update balise

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=G-LGCXZQVT0R"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LGCXZQVT0R"></script>
 <script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'G-LGCXZQVT0R');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LGCXZQVT0R');
 </script>
  """
  return prefix_google + "Hello World"
