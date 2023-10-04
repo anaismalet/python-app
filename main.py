7df092a23f5078e33098ff9667fc046f1a2c549b
anaismalet <anais.malet55@gmail.com>
2023-10-04 12:55:15 +0200

    Textbox update

--- main.py
+++ main.py
@@ -1,45 +1,61 @@
-
-
-from flask import Flask
+from flask import Flask, request
 import logging
 
+# Flask app
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
-
 def hello_world():
- # Adding a google tag for google analysis
- prefix_google = """
- <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LGCXZQVT0R"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LGCXZQVT0R');
-</script>
- """
- # Return the tag and a print "Hello world"
- return prefix_google + "Hello World"
-
-@app.route("/logger")
-def logger():
-    
-    # print a message on the logger page
-    log = "Logger page"
-
-    # print a message in Python, check in deta micro
-    app.logger.info("Message python : connexion page logger")
-
-    # print a message in the browser console, check in console browser
-    browser_log = """
+    # Adding a google tag for google analysis
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LGCXZQVT0R"></script>
     <script>
-        console.log('Message in browser : connexion à la page Logger');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-LGCXZQVT0R');
     </script>
     """
+    # Return the tag and a print "Hello world"
+    return prefix_google + "Hello World"
 
-    return  browser_log + log
+# Route to logger page
+@app.route("/logger", methods=['GET', 'POST'])
+def log():
+    # Print a message in Python
+    log_msg = "Logger page"
+    app.logger.info(log_msg)
 
+    if request.method == 'POST':
+        # Retreiived the text in the text box
+        text_from_textbox = request.form['textbox']
+
+        # Print a message in the browser console with the text from the text box
+        browser_log = f"""
+        <script>
+            console.log('Console du web browser : Vous êtes bien connectés à la page des logs');
+            console.log('Texte de la boîte de texte : {text_from_textbox}');
+        </script>
+        """
+    else:
+        # Print a message in the browser console
+        browser_log = """
+        <script>
+            console.log('Console du web browser : Vous êtes bien connectés à la page des logs');
+        </script>
+        """
+
+    # Formulaire HTML avec une boîte de texte
+    textbox_form = """
+    <form method="POST">
+        <label for="textbox">Text Box :</label><br>
+        <input type="text" id="textbox" name="textbox"><br><br>
+        <input type="submit" value="Soumettre">
+    </form>
+    """
+
+    return log_msg + browser_log + textbox_form
